72d071228c26c1e1d6bec98b109ccf32a5e7d1b5
GlenWong97 <shuzewong@yahoo.com.sg>
2019-08-24 10:37:53 +0800

    Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Duke {
     public static void main(String[] args) {
-        
+
         String bracket = "____________________________________________________________\n";
         String intro = " Hello! I'm Duke\n" +
                 " What can I do for you?\n";
@@ -11,12 +11,21 @@ public class Duke {
 
         Scanner myObj = new Scanner(System.in);
         String inputMessage = myObj.nextLine();
+        ArrayList<String> tasks = new ArrayList<String>();
 
         while (!inputMessage.equals("bye")) {
-            System.out.println(bracket + " " + inputMessage + "\n" + bracket);
+            if (inputMessage.equals("list")) {
+                System.out.print(bracket);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(" " + (i+1) + ". " + tasks.get(i));
+                }
+                System.out.print(bracket);
+            } else {
+                System.out.print(bracket + " added: " + inputMessage + "\n"  + bracket);
+                tasks.add(inputMessage);
+            }
             inputMessage = myObj.nextLine();
         }
-
         System.out.println(bracket + " " + "Bye. Hope to see you again soon!" + "\n" + bracket);
     }
 }
